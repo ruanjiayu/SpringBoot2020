7faa7a6f569b74069c90c3ff58973ee3a702bd61
summer <ruanjiayu@fenxianglife.com>
2020-07-20 15:15:40 +0800

    新增一个时间拦截器

---
+++ src/main/java/com/fun/uncle/springboot2020/config/TimeInterceptor.java
@@ -0,0 +1,45 @@
+package com.fun.uncle.springboot2020.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description: 时间拦截器
+ * @Author: Summer
+ * @DateTime: 2020/7/20 2:58 下午
+ * @Version: 0.0.1-SNAPSHOT
+ */
+@Slf4j
+@Component
+public class TimeInterceptor implements HandlerInterceptor {
+
+    private ThreadLocal<Long> threadLocalStart = new ThreadLocal<>();
+    private ThreadLocal<Long> threadLocalEnd = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTIme = System.currentTimeMillis();
+        threadLocalStart.set(startTIme);
+        log.info("开始时间：{}", startTIme);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long endTIme = System.currentTimeMillis();
+        threadLocalEnd.set(endTIme);
+        log.info("结束时间：{}", endTIme);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = threadLocalStart.get();
+        long endTime = threadLocalEnd.get();
+        log.info("接口执行时间：{} 毫秒", endTime - startTime);
+    }
+}
--- src/main/java/com/fun/uncle/springboot2020/config/WebConfigurer.java
+++ src/main/java/com/fun/uncle/springboot2020/config/WebConfigurer.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @Description:
+ * @Description: mvc 控制器
  * @Author: Summer
  * @DateTime: 2020/7/20 2:45 下午
  * @Version: 0.0.1-SNAPSHOT
@@ -18,8 +18,21 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Autowired
     private LogInterceptor logInterceptor;
 
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+    /**
+     * 类似与栈的效果，先进后出
+     * 正向迭代 org.springframework.web.servlet.DispatcherServlet#doDispatch(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     * 逆向迭代 org.springframework.web.servlet.HandlerExecutionChain#applyPreHandle(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     * 逆向迭代 org.springframework.web.servlet.HandlerExecutionChain#applyPostHandle(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, org.springframework.web.servlet.ModelAndView)
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(logInterceptor);
+        registry.addInterceptor(timeInterceptor);
     }
+
+
 }
