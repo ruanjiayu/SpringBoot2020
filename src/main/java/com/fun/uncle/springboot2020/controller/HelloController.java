62c03015c798ced792975ec2c452d269c7764724
summer <ruanjiayu@fenxianglife.com>
2020-07-20 11:42:00 +0800

    完成对于Spring Boot项目的swagger配置

--- src/main/java/com/fun/uncle/springboot2020/controller/HelloController.java
+++ src/main/java/com/fun/uncle/springboot2020/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.fun.uncle.springboot2020.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: 0.0.1-SNAPSHOT
  */
 @RestController
+@Api(tags = "hello 控制层")
 public class HelloController {
 
     @GetMapping
-    public String hello() {
+    @ApiOperation("Hello Spring Boot 方法")
+    public String hello(@RequestParam(value = "name", required = false) String name) {
+        if (StringUtils.isNotBlank(name)) {
+            return name + " Hello Spring Boot";
+        }
         return "Hello Spring Boot";
     }
 }
