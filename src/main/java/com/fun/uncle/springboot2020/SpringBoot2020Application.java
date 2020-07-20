5b6073bce105319f3a8598ade18155fcdd4be336
summer <ruanjiayu@fenxianglife.com>
2020-07-20 11:13:12 +0800

    添加上版本控制

---
+++ src/main/java/com/fun/uncle/springboot2020/SpringBoot2020Application.java
@@ -0,0 +1,13 @@
+package com.fun.uncle.springboot2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2020Application.class, args);
+    }
+
+}
