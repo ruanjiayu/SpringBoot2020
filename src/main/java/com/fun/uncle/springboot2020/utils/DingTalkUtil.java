d98cc8292403407d7766f06aabef4a5cfb8174f7
summer <ruanjiayu@fenxianglife.com>
2020-10-13 15:16:30 +0800

    钉钉机器人待完成

---
+++ src/main/java/com/fun/uncle/springboot2020/utils/DingTalkUtil.java
@@ -0,0 +1,12 @@
+package com.fun.uncle.springboot2020.utils;
+
+/**
+ * @Description: 钉钉机器人工具类
+ * @Author: Summer
+ * @DateTime: 2020/10/13 2:54 下午
+ * @Version: 0.0.1-SNAPSHOT
+ */
+public class DingTalkUtil {
+
+
+}
